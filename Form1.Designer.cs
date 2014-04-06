f486aea9af3c0f28785320ddf28c42d4c6185112
Wsheerio <trivial.derp@gmail.com>
2014-04-06 07:49:10 -0400

    More options

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,6 +41,15 @@
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -77,7 +86,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Video / GIF";
+            this.button1.Text = "Video";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // label1
@@ -141,19 +150,101 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(68, 168);
+            this.button3.Location = new System.Drawing.Point(117, 277);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 8;
+            this.button3.TabIndex = 13;
             this.button3.Text = "Go";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 168);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(133, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Threads";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(12, 194);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(118, 244);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Output";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(12, 220);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(138, 223);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Adjust";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(12, 246);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(137, 197);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Sound";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 274);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(105, 26);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "moot, please remove\r\nlength restrictions.";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(204, 201);
+            this.ClientSize = new System.Drawing.Size(204, 310);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox7);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox6);
@@ -193,6 +284,15 @@
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -11,29 +11,50 @@ namespace webm
 {
     public partial class Form1 : Form
     {
+        double time;
+        int fixer;
+        string command;
         public Form1()
         {
             InitializeComponent();
         }
         private void button3_Click(object sender, EventArgs e)
         {
+            command = "";
+            fixer = Convert.ToInt32(textBox9.Text);
             string[] startTime = textBox5.Text.Split(':');
             string[] endTime = textBox6.Text.Split(':');
-            double time = (Convert.ToDouble(endTime[0]) - Convert.ToDouble(startTime[0])) * 60 * 60 + (Convert.ToDouble(endTime[1]) - Convert.ToDouble(startTime[1])) * 60 + (Convert.ToDouble(endTime[2]) - Convert.ToDouble(startTime[2]));
-            string command = "-i " + textBox1.Text + " -ss " + textBox5.Text + " -to " + textBox6.Text + " -b:v " + Convert.ToString((Convert.ToDouble(textBox3.Text) * 1024 * 8 * 0.975) / time);
+            time = (Convert.ToDouble(endTime[0]) - Convert.ToDouble(startTime[0])) * 60 * 60 + (Convert.ToDouble(endTime[1]) - Convert.ToDouble(startTime[1])) * 60 + (Convert.ToDouble(endTime[2]) - Convert.ToDouble(startTime[2]));
+            command = "-y -i " + textBox1.Text + " -ss " + textBox5.Text + " -to " + textBox6.Text;
             if (textBox2.Text != "")
             {
                 command += " -vf \"ass=" + textBox2.Text + "\"";
             }
-            command += " -s " + textBox4.Text + " -an -quality good -cpu-used 0 -c:v libvpx -slices 8 -auto-alt-ref 1 -f webm -pass ";
+            if (textBox4.Text != "")
+            {
+                command += " -s " + textBox4.Text;
+            }
+            if (textBox8.Text != "")
+            {
+                command += " -ac 1 -b:v " + Convert.ToString(((Convert.ToDouble(textBox3.Text) * 8192) / time) + fixer - Convert.ToDouble(textBox8.Text)) + "k -b:a " + textBox8.Text + "k";
+            }
+            else
+            {
+                command += " -an -b:v " + Convert.ToString(((Convert.ToDouble(textBox3.Text) * 8192) / time) + fixer) + "k";
+            }
+            if (textBox7.Text != "")
+            {
+                command += " -threads " + textBox7.Text;
+            }
+            command += " -quality best -cpu-used 0 -c:v libvpx -c:a libvorbis -slices 8 -auto-alt-ref 1 -pass ";
             Process proc = new Process();
-            proc.StartInfo.FileName = "ffmpeg.exe";
-            proc.StartInfo.Arguments = command + "1 null";
+            proc.StartInfo.FileName = "ffmpeg";
+            proc.StartInfo.Arguments = command + "1 " + textBox10.Text;
             proc.Start();
             proc.WaitForExit();
             proc = new Process();
-            proc.StartInfo.FileName = "ffmpeg.exe";
-            proc.StartInfo.Arguments = command + "2 output.webm";
+            proc.StartInfo.FileName = "ffmpeg";
+            proc.StartInfo.Arguments = command + "2 " + textBox10.Text;
             proc.Start();
             proc.WaitForExit();
         }
