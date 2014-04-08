b38369e54980d5cc2ff1b7989302998ddb605942
Wsheerio <trivial.derp@gmail.com>
2014-04-08 03:10:02 -0400

    a very large amount of changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -54,6 +54,7 @@
             // 
             // textBox1
             // 
+            this.textBox1.AllowDrop = true;
             this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
@@ -61,6 +62,7 @@
             // 
             // textBox2
             // 
+            this.textBox2.AllowDrop = true;
             this.textBox2.Location = new System.Drawing.Point(12, 38);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
@@ -68,17 +70,19 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 64);
+            this.textBox3.Location = new System.Drawing.Point(12, 116);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 4;
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "3";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(12, 90);
+            this.textBox4.Location = new System.Drawing.Point(12, 142);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 5;
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "-1:-1";
             // 
             // button1
             // 
@@ -93,7 +97,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(130, 67);
+            this.label1.Location = new System.Drawing.Point(130, 119);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(51, 13);
             this.label1.TabIndex = 5;
@@ -102,7 +106,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(127, 93);
+            this.label2.Location = new System.Drawing.Point(127, 145);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 13);
             this.label2.TabIndex = 6;
@@ -111,7 +115,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(128, 119);
+            this.label3.Location = new System.Drawing.Point(128, 67);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(55, 13);
             this.label3.TabIndex = 7;
@@ -120,7 +124,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(130, 145);
+            this.label4.Location = new System.Drawing.Point(130, 93);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(52, 13);
             this.label4.TabIndex = 8;
@@ -128,18 +132,18 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(12, 116);
+            this.textBox5.Location = new System.Drawing.Point(12, 64);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 6;
+            this.textBox5.TabIndex = 4;
             this.textBox5.Text = "00:00:00";
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(12, 142);
+            this.textBox6.Location = new System.Drawing.Point(12, 90);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(100, 20);
-            this.textBox6.TabIndex = 7;
+            this.textBox6.TabIndex = 5;
             // 
             // button2
             // 
@@ -163,26 +167,26 @@
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(12, 168);
+            this.textBox7.Location = new System.Drawing.Point(12, 220);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(100, 20);
-            this.textBox7.TabIndex = 8;
+            this.textBox7.TabIndex = 10;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(133, 171);
+            this.label5.Location = new System.Drawing.Point(128, 223);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.Size = new System.Drawing.Size(56, 13);
             this.label5.TabIndex = 10;
-            this.label5.Text = "Threads";
+            this.label5.Text = "Advanced";
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(12, 194);
+            this.textBox8.Location = new System.Drawing.Point(12, 168);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(100, 20);
-            this.textBox8.TabIndex = 9;
+            this.textBox8.TabIndex = 8;
             // 
             // button4
             // 
@@ -196,20 +200,19 @@
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(12, 220);
+            this.textBox9.Location = new System.Drawing.Point(12, 194);
             this.textBox9.Name = "textBox9";
             this.textBox9.Size = new System.Drawing.Size(100, 20);
-            this.textBox9.TabIndex = 10;
-            this.textBox9.Text = "0";
+            this.textBox9.TabIndex = 9;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(138, 223);
+            this.label6.Location = new System.Drawing.Point(141, 197);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.Size = new System.Drawing.Size(29, 13);
             this.label6.TabIndex = 14;
-            this.label6.Text = "Adjust";
+            this.label6.Text = "Crop";
             // 
             // textBox10
             // 
@@ -221,7 +224,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(137, 197);
+            this.label7.Location = new System.Drawing.Point(137, 171);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(38, 13);
             this.label7.TabIndex = 16;
--- Form1.cs
+++ Form1.cs
@@ -8,24 +8,28 @@ using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
+using System.IO;
 namespace webm
 {
     public partial class Form1 : Form
     {
-        double time;
-        int fixer;
         string command;
+        double fixer = 0;
         public Form1()
         {
             InitializeComponent();
+            textBox1.DragDrop += new DragEventHandler(textBox1_DragDrop);
+            textBox2.DragDrop += new DragEventHandler(textBox2_DragDrop);
+            textBox1.DragEnter += new DragEventHandler(MyTextBox_DragEnter);
+            textBox2.DragEnter += new DragEventHandler(MyTextBox_DragEnter);
         }
         private void button3_Click(object sender, EventArgs e)
         {
-            fixer = Convert.ToInt32(textBox9.Text);
-            string[] startTime = textBox5.Text.Split(':');
-            string[] endTime = textBox6.Text.Split(':');
-            time = (Convert.ToDouble(endTime[0]) - Convert.ToDouble(startTime[0])) * 60 * 60 + (Convert.ToDouble(endTime[1]) - Convert.ToDouble(startTime[1])) * 60 + (Convert.ToDouble(endTime[2]) - Convert.ToDouble(startTime[2]));
-            command = "-y -i " + "\"" + textBox1.Text + "\"" + " -ss " + textBox5.Text + " -to " + textBox6.Text;
+            command = "-y -i " + "\"" + textBox1.Text + "\"" + " -ss " + textBox5.Text + " -t " + textBox6.Text;
+            if (textBox4.Text != "")
+            {
+                command += " -vf scale=" + textBox4.Text;
+            }
             if (textBox2.Text != "")
             {
                 string[] noEscape = Regex.Split(textBox2.Text, string.Empty);
@@ -40,30 +44,27 @@ namespace webm
                         noEscape[i] = "\\\\:";
                     }
                 }
-                command += " -vf \"ass=";
+                command += ",\"ass=";
                 for (int i = 0; i < noEscape.Length; i++)
                 {
                     command += noEscape[i];
                 }
                 command += "\"";
             }
-            if (textBox4.Text != "")
+            if (textBox9.Text != "")
             {
-                command += " -s " + textBox4.Text;
+                command += ",crop=" + textBox9.Text;
             }
             if (textBox8.Text != "")
             {
-                command += " -ac 1 -b:v " + Convert.ToString(((Convert.ToDouble(textBox3.Text) * 8192 + fixer) / time) - Convert.ToDouble(textBox8.Text)) + "k -b:a " + textBox8.Text + "k";
+                command += " -ac 1 -b:v " + Convert.ToString(((Convert.ToDouble(textBox3.Text) * 8192 - Convert.ToDouble(textBox8.Text) * Convert.ToDouble(textBox3.Text)) / Convert.ToDouble(textBox6.Text)) + (fixer * 8 / Convert.ToDouble(textBox6.Text))) + "k -b:a " + textBox8.Text + "k";
             }
             else
             {
-                command += " -an -b:v " + Convert.ToString((Convert.ToDouble(textBox3.Text) * 8192 + fixer) / time) + "k";
+                command += " -an -b:v " + Convert.ToString(((Convert.ToDouble(textBox3.Text) * 8192) / Convert.ToDouble(textBox6.Text)) + (fixer * 8 / Convert.ToDouble(textBox6.Text))) + "k";
             }
-            if (textBox7.Text != "")
-            {
-                command += " -threads " + textBox7.Text;
-            }
-            command += " -quality best -cpu-used 0 -c:v libvpx -c:a libvorbis -slices 8 -auto-alt-ref 1 -pass ";
+            command += " " + textBox7.Text;
+            command += " -threads " + Convert.ToString(Environment.ProcessorCount) + " -quality best -cpu-used 0 -c:v libvpx -c:a libvorbis -slices 8 -auto-alt-ref 1 -lag-in-frames 25 -pass ";
             Process proc = new Process();
             proc.StartInfo.FileName = "ffmpeg";
             proc.StartInfo.Arguments = command + "1 " + "\"" + textBox10.Text + "\"";
@@ -75,8 +76,8 @@ namespace webm
             proc.Start();
             proc.WaitForExit();
             System.IO.File.Delete("ffmpeg2pass-0.log");
+            fixer += (Convert.ToDouble(textBox3.Text) * 1024 * 1024 - new FileInfo(textBox10.Text).Length) / 1024;
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog browse = new OpenFileDialog();
@@ -89,6 +90,11 @@ namespace webm
 
         private void button2_Click(object sender, EventArgs e)
         {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "ffmpeg";
+            proc.StartInfo.Arguments = "-y -i " + textBox1.Text + " -c:s copy " + Path.GetDirectoryName(textBox1.Text) + "\\sub.ass";
+            proc.Start();
+            proc.WaitForExit();
             OpenFileDialog browse = new OpenFileDialog();
             browse.Filter = "ass (*.ass)|*.ass|All Files (*.*)|*.*";
             browse.FilterIndex = 1;
@@ -108,6 +114,44 @@ namespace webm
             if (browse.FileName != "")
             {
                 textBox10.Text = browse.FileName;
+            }
+        }
+        void MyTextBox_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+        void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            DataObject data = (DataObject)e.Data;
+            if (data.ContainsFileDropList())
+            {
+                string[] rawFiles = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (rawFiles != null)
+                {
+                    List<string> lines = new List<string>();
+                    foreach (string path in rawFiles)
+                    {
+                        lines.Add(path);
+                    }
+                    textBox1.Text = lines[0];
+                }
+            }
+        }
+        void textBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            DataObject data = (DataObject)e.Data;
+            if (data.ContainsFileDropList())
+            {
+                string[] rawFiles = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (rawFiles != null)
+                {
+                    List<string> lines = new List<string>();
+                    foreach (string path in rawFiles)
+                    {
+                        lines.Add(path);
+                    }
+                    textBox2.Text = lines[0];
+                }
             }
         }
     }
